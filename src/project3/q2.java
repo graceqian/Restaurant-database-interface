cec6eee8fe726335383ac9f734fff48407130fe3
graceqian <graceqian125@gmail.com>
2020-04-11 15:08:49 -0400

    finished q2

--- src/project3/q2.java
+++ src/project3/q2.java
@@ -1,15 +1,18 @@
 package project3;
 import java.math.BigDecimal;
+import java.util.Scanner;
 import java.sql.*;
+import java.sql.Date;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Scanner;
 
 public class q2 {
 
 	public static void main(String args[]) throws SQLException{//idk where to put this
 		
-		
 		// Unique table names.  Either the user supplies a unique identifier as a command line argument, or the program makes one up.
-		String tableName = "";
         int sqlCode=0;      // Variable to hold SQLCODE
         String sqlState="00000";  // Variable to hold SQLSTATE
 	        
@@ -107,7 +110,7 @@ public class q2 {
 				    while ( rs.next ( ) ) {
 				    System.out.println();
 					String vendor = rs.getString (4) ;//third col
-					String price = rs.getString (5);//fourth col
+					String price = rs.getString (5).substring(0, rs.getString(5).length() - 4);//fourth col
 					System.out.println ("the lowest price for " + qty + " units of "+ item + " is $" + price + " from the vendor " + vendor);
 				    }
 				    System.out.println ("DONE\n");
@@ -141,7 +144,7 @@ public class q2 {
 				    System.out.println("#" + num++);
 					int menuid = rs.getInt ( 1 ) ;//first col
 					int orderamt = rs.getInt (2);//second col
-					System.out.println ("menuid:  " + menuid);//should we output the item name instead?
+					System.out.println ("menu item id:  " + menuid);//should we output the item name instead?
 					System.out.println ("amount ordered:  " + orderamt);
 				    }
 				    System.out.println ("DONE");
@@ -157,14 +160,150 @@ public class q2 {
         	}//end of if - user picks C
         	
         	
+        	//USER PICKS D
+        	else if(optionPicked.equalsIgnoreCase("D")) {
+        		try {
+		//		    String querySQL = "SELECT id, name from " + tableName + " WHERE NAME = \'Vicki\'";
+					String updateSQL = "UPDATE menuitem mi\n" + 
+							"SET price = ( SELECT price FROM  (\n" + 
+							"SELECT mi.mi_id as mi_id, mi.name as name, tp.TotalPrice as price, mi.description as description FROM menuitem mi, (\n" + 
+							"SELECT menurecipe.mi_id, ROUND(2 * 1.30 * SUM(recipecallsfor.qty * a.price), 0) / 2 as TotalPrice \n" + 
+							"FROM menurecipe, recipecallsfor, (\n" + 
+							"SELECT poi.item_name, price, date_time \n" + 
+							"FROM purchaseorderitem poi INNER JOIN (\n" + 
+							"SELECT item_name, order_id, date_time FROM purchaseorder po INNER JOIN (\n" + 
+							"SELECT item_name, MAX(date_time) AS newest FROM (\n" + 
+							"SELECT poi.rname as rname, poi.item_name as item_name, po.order_id as order_id, poi.price as price, po.date_time as date_time \n" + 
+							"FROM purchaseorder po INNER JOIN ( SELECT * FROM purchaseorderitem ) poi \n" + 
+							"ON po.order_id = poi.order_id)	\n" + 
+							"GROUP BY item_name) pip ON po.date_time = pip.newest) ms \n" + 
+							"ON poi.item_name = ms.item_name AND poi.order_id = ms.order_id) as a \n" + 
+							"WHERE menurecipe.recipe_id = recipecallsfor.recipe_id AND recipecallsfor.item_name = a.item_name\n" + 
+							"GROUP BY menurecipe.mi_id ORDER BY menurecipe.mi_id) tp WHERE mi.mi_id = tp.mi_id) newvals WHERE mi.mi_id = newvals.mi_id)";
+		//		   
+					statement.executeUpdate(updateSQL);
+					
+					//find top five most expensive menu items
+					String querySQL = "SELECT *FROM menuitem ORDER BY price DESC FETCH FIRST 5 ROWS ONLY";
+				    System.out.println("\nFive most expensive menu items:");
+				    java.sql.ResultSet rs = statement.executeQuery ( querySQL ) ;
+				    int num = 1;
+				    while ( rs.next ( ) ) {
+				    System.out.println();
+				    System.out.println("#" + num++);
+					int menuid = rs.getInt ( 1 ) ;//first col
+					String itemName = rs.getString (2);//second col
+					String price = rs.getString(4).substring(0, rs.getString(4).length() - 6);
+			
+					System.out.println ("menu item id:  " + menuid);//should we output the item name instead?
+					System.out.println("menu item name: "+ itemName);
+					System.out.println ("price:  $" + price);
+				    }
+				    System.out.println ("DONE\n");
+				} catch (SQLException e)
+				    {
+					sqlCode = e.getErrorCode(); // Get SQLCODE
+					sqlState = e.getSQLState(); // Get SQLSTATE
+			                
+					// Your code to handle errors comes here;
+					// something more meaningful than a print would be good
+					System.out.println("Code: " + sqlCode + "  sqlState: " + sqlState);
+				    }
+        	}//end of option D else if
+        	
+        	
+        	//USER PICKS E
+        	else if(optionPicked.equalsIgnoreCase("E")) {
+        		try {
+        			PreparedStatement preparedStatement= con.prepareStatement("SELECT *"
+        					+ "FROM Reservation "
+        					+ "WHERE phone_num = ? AND start_time = ?");
+        			
+        			Scanner in = new Scanner(System.in);
+        			System.out.print("Enter phone number of reservation: ");
+        			String phonenum = in.nextLine();
+        			System.out.print("Enter date of reservation (YYYY-MM-DD):");
+        			String datestr = in.nextLine();
+        			System.out.print("Enter time of reservation(HH.MM):");
+        			String timestr= in.nextLine();
+        			
+//        			int year, month, day, hour, minute;
+//        			year=Integer.parseInt(datestr.substring(0,4));
+//        			month=Integer.parseInt(datestr.substring(5,7));
+//        			day=Integer.parseInt(datestr.substring(8));
+//        			hour = Integer.parseInt(timestr.substring(0,2));
+//        			minute = Integer.parseInt(timestr.substring(4));
+        			
+        			//parse the starttime input and turn it into a Timestamp
+        			DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH.mm");
+        			java.util.Date date = null;
+					try {
+						date = dateFormat.parse(datestr + " " + timestr);
+					} catch (ParseException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+        			long time = date.getTime();
+        			Timestamp starttime = new Timestamp(time);
+        			
+        			preparedStatement.setString(1, phonenum);
+        			preparedStatement.setTimestamp(2, starttime);
+        			
+        			
+        			
+        			//INSERT INTO reservation (customer_id,rname, table_num,name, phone_num, start_time, end_time) 
+//        			VALUES ( 168,'Le Mezz',4,'Maria Joe','514525474','2020-02-29-13.30.00.000000', '2020-02-29-14.23.00.000000')
+        			
+        			//Select * from reservation where phone_num = '514525474' AND start_time='2020-02-29-13.30.00.000000'
+        			
+        			
+
+				    System.out.println("\nReservation(s) deleted:");
+				    java.sql.ResultSet rs = preparedStatement.executeQuery();
+				    int num = 1;
+				    while ( rs.next ( ) ) {
+				    System.out.println();
+				    System.out.println("#" + num++);
+					int custid = rs.getInt ( 1 ) ;//first col
+					String restaurant = rs.getString (2);//second col
+					int tablenum = rs.getInt(3);
+					String name = rs.getString(4);
+			
+					System.out.println ("customer id: " + custid);//should we output the item name instead?
+					System.out.println("restaurant: "+ restaurant);
+					System.out.println ("customer name: " + name);
+					System.out.println("phone number: "+ phonenum);
+					System.out.println("table number: " + tablenum);
+					System.out.println("date and time of reservation: " + datestr +" "+ timestr);
+				    }
+				    PreparedStatement update = con.prepareStatement("DELETE FROM reservation "
+				    		+ "WHERE phone_num = ? AND start_time = ?");
+					
+					update.setString(1, phonenum);
+        			update.setTimestamp(2, starttime);
+				    update.executeUpdate();//delete the rows	
+				    
+				    System.out.println ("DONE\n");
+				} catch (SQLException e)
+				    {
+					sqlCode = e.getErrorCode(); // Get SQLCODE
+					sqlState = e.getSQLState(); // Get SQLSTATE
+			                
+					// Your code to handle errors comes here;
+					// something more meaningful than a print would be good
+					System.out.println("Code: " + sqlCode + "  sqlState: " + sqlState);
+				    }
+        	}
+        	
 			//USER PICKS Q
 			else if(optionPicked.equalsIgnoreCase("Q")) {
+				System.out.println("Session Ended");
 				break;
 			}
 				
 			//INVALID INPUT
 			else {
-				System.out.println("Invalid Input. Please type the letter of one of the options listed below.");
+				System.out.println("Invalid Input. Please type the letter of one of the options listed below.\n");
 			}
         }
 		
@@ -177,8 +316,8 @@ public class q2 {
 		System.out.print("Pick an option:\n" +
 				"A. Determine which tables in the restaurant are currently free\n"
 				+ "B. Find lowest price of a specific inventory item for a desired quantity\n"
-				+ "C. Determine most popular items in the past 5 months, and order them by popularity\n"
-				+ "D. Recalculate all menu items based on cost, with a markup of 30%\n"
+				+ "C. Rank the most popular items in the past 5 months, and order them by popularity\n"
+				+ "D. Recalculate all menu items based on latest purchase costs from vendors, with a markup of 30%,\n   then view the 5 most expensive menu items and their prices\n"
 				+ "E. Delete all the reservations on all tables for a given phone number and date\n"
 				+ "Q. Quit\n"
 				+ "Enter A/B/C/D/E/Q: ");
